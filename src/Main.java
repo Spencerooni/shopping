6ab4dd4e7514167ecf461821f99946d552ecf8bc
David Spence <d.spence@kainos.com>
2015-09-25 10:02:35 +0100

    Adding basic shopping task

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] itemsForSale = { "shed", "handbag", "tractor" };
+
+        System.out.println("What would you like to buy?");
+
+        for (int i = 0; i < itemsForSale.length; i++)
+        {
+            System.out.println(i + 1 + ". " + itemsForSale[i]);
+        }
+
+	    Scanner scanner = new Scanner(System.in);
+
+        int selectedItemToOrder = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("How many do you want to order?");
+
+        int numberToOrder = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] addresses = new String[numberToOrder];
+
+        for (int i = 0; i < numberToOrder; i++)
+        {
+            System.out.println("Enter address for person " + (i + 1));
+            String address = scanner.nextLine();
+            addresses[i] = address;
+        }
+
+        System.out.println("Thanks! Sending " + numberToOrder + " " + itemsForSale[selectedItemToOrder] + "(s) to the following addresses: ");
+
+        for (int i = 0; i < addresses.length; i++)
+        {
+            System.out.println(addresses[i]);
+        }
+
+    }
+}
