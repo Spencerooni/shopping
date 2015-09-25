0524293ccae03a3885b5f097426b7fe78ed81dd6
David Spence <d.spence@kainos.com>
2015-09-25 16:07:02 +0100

    Adding endless entry for addresses

--- src/Main.java
+++ src/Main.java
@@ -18,21 +18,32 @@ public class Main {
         int selectedItemToOrder = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("How many do you want to order?");
+        String[] addresses = new String[0];
+        boolean enteringAddresses = true;
 
-        int numberToOrder = scanner.nextInt();
-        scanner.nextLine();
-
-        String[] addresses = new String[numberToOrder];
-
-        for (int i = 0; i < numberToOrder; i++)
+        while (enteringAddresses)
         {
-            System.out.println("Enter address for person " + (i + 1));
-            String address = scanner.nextLine();
-            addresses[i] = address;
+            String newAddress = scanner.nextLine();
+            if (newAddress == null || newAddress.isEmpty())
+            {
+                enteringAddresses = false;
+            }
+            else
+            {
+                String[] copyOfAddresses = new String[addresses.length + 1];
+
+                for (int i = 0; i < addresses.length; i++)
+                {
+                    copyOfAddresses[i] = addresses[i];
+                }
+
+                copyOfAddresses[copyOfAddresses.length - 1] = newAddress;
+
+                addresses = copyOfAddresses;
+            }
         }
 
-        System.out.println("Thanks! Sending " + numberToOrder + " " + itemsForSale[selectedItemToOrder] + "(s) to the following addresses: ");
+        System.out.println("Thanks! Sending " + addresses.length + " " + itemsForSale[selectedItemToOrder] + "(s) to the following addresses: ");
 
         for (int i = 0; i < addresses.length; i++)
         {
